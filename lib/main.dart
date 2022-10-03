2f789cc17b800a7c190a1ece668eeaaa09775cda
DrAaGoN <DrAaGoN@gmail.com>
2022-10-03 23:13:11 +0200

    new main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Match game',
+      title: 'Home Page',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
