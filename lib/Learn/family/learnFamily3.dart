a955cb415a39de5d7f1b77f922ff530a8b43f76f
mazen <mazenmohamed262686@gmail.com>
2022-10-09 19:27:07 +0200

    comment

--- lib/Learn/family/learnFamily3.dart
+++ lib/Learn/family/learnFamily3.dart
@@ -45,7 +45,7 @@ class learnFamily3 extends StatelessWidget {
             child: Stack(children: [
           Positioned(
             right: 80,
-            top: 10,
+            top: 25,
             child: Material(
               color: const Color.fromARGB(255, 10, 79, 135),
               elevation: 8,
@@ -60,8 +60,8 @@ class learnFamily3 extends StatelessWidget {
                   Ink.image(
                     image: const AssetImage(
                         "assets/images/learn/family/grandFather.png"),
-                    height: 190,
-                    width: 200,
+                    height: 220,
+                    width: 240,
                     fit: BoxFit.cover,
                   ),
                   const SizedBox(
@@ -80,7 +80,7 @@ class learnFamily3 extends StatelessWidget {
           ),
           Positioned(
             right: 80,
-            top: 275,
+            top: 370,
             child: Material(
               color: const Color.fromARGB(255, 10, 79, 135),
               elevation: 8,
@@ -95,8 +95,8 @@ class learnFamily3 extends StatelessWidget {
                   Ink.image(
                     image: const AssetImage(
                         "assets/images/learn/family/grandMother.png"),
-                    height: 190,
-                    width: 200,
+                    height: 220,
+                    width: 240,
                     fit: BoxFit.cover,
                   ),
                   const SizedBox(
@@ -115,7 +115,7 @@ class learnFamily3 extends StatelessWidget {
           ),
           Positioned(
             left: 10,
-            top: 235,
+            top: 310,
             child: FloatingActionButton(
                 onPressed: () {
                   Navigator.push(
@@ -127,7 +127,7 @@ class learnFamily3 extends StatelessWidget {
           ),
           Positioned(
             right: 10,
-            top: 0,
+            top: 10,
             child: FloatingActionButton(
                 onPressed: () {
                   Navigator.push(
