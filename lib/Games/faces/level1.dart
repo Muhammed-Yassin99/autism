85de65298ebf9d44ba98a69500a8bc95b188fc42
DrAaGoN <DrAaGoN@gmail.com>
2022-10-14 23:49:04 +0200

    commit

--- lib/Games/faces/level1.dart
+++ lib/Games/faces/level1.dart
@@ -97,188 +97,196 @@ class _HomeScreenState extends State<facesLevel1> {
       initGame2();
     }
     return Scaffold(
-      body: Container(
-        color: Colors.teal,
-        child: SingleChildScrollView(
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              ConfettiWidget(
-                confettiController: controller,
-                shouldLoop: false,
-                blastDirectionality: BlastDirectionality.explosive,
-                numberOfParticles: 20,
-                emissionFrequency: 0.09,
-                gravity: 1,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(
-                            text: '$score',
-                            style: const TextStyle(
-                              fontSize: 32,
-                              color: Colors.black,
+        body: SafeArea(
+            child: Stack(
+      children: [
+        Container(
+          color: Colors.teal,
+          height: MediaQuery.of(context).size.height * 1,
+          width: MediaQuery.of(context).size.width * 1,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                ConfettiWidget(
+                  confettiController: controller,
+                  shouldLoop: false,
+                  blastDirectionality: BlastDirectionality.explosive,
+                  numberOfParticles: 20,
+                  emissionFrequency: 0.09,
+                  gravity: 1,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '$score',
+                              style: const TextStyle(
+                                fontSize: 32,
+                                color: Colors.black,
+                              ),
                             ),
-                          ),
-                          const TextSpan(
-                            text: ' :النقاط',
-                            style: TextStyle(
-                              fontSize: 32,
-                              color: Colors.black,
+                            const TextSpan(
+                              text: ' :النقاط',
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: Colors.black,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  if (!gameOver)
-                    Row(
-                      children: [
-                        const Spacer(),
-                        Column(
-                          children: items.map((item) {
-                            return Container(
-                              margin: const EdgeInsets.all(8),
-                              child: Draggable<ItemModel>(
-                                data: item,
-                                childWhenDragging: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  backgroundImage: AssetImage(item.img),
-                                  radius: 50,
+                    if (!gameOver)
+                      Row(
+                        children: [
+                          const Spacer(),
+                          Column(
+                            children: items.map((item) {
+                              return Container(
+                                margin: const EdgeInsets.all(8),
+                                child: Draggable<ItemModel>(
+                                  data: item,
+                                  childWhenDragging: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    backgroundImage: AssetImage(item.img),
+                                    radius: 50,
+                                  ),
+                                  feedback: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    backgroundImage: AssetImage(item.img),
+                                    radius: 50,
+                                  ),
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    backgroundImage: AssetImage(item.img),
+                                    radius: 50,
+                                  ),
                                 ),
-                                feedback: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  backgroundImage: AssetImage(item.img),
-                                  radius: 50,
-                                ),
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  backgroundImage: AssetImage(item.img),
-                                  radius: 50,
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        const Spacer(flex: 1),
-                        Column(
-                          children: items2.map((item) {
-                            return DragTarget<ItemModel>(
-                              onAccept: (receivedItem) {
-                                if (item.value == receivedItem.value) {
+                              );
+                            }).toList(),
+                          ),
+                          const Spacer(flex: 1),
+                          Column(
+                            children: items2.map((item) {
+                              return DragTarget<ItemModel>(
+                                onAccept: (receivedItem) {
+                                  if (item.value == receivedItem.value) {
+                                    setState(() {
+                                      items.remove(receivedItem);
+                                      items2.remove(item);
+                                    });
+                                    score += 10;
+                                    item.accepting = false;
+                                    playBeep('sounds/true.wav');
+                                    controller.play();
+                                    Timer(const Duration(seconds: 1),
+                                        (() => controller.stop()));
+                                  } else {
+                                    setState(() {
+                                      score -= 5;
+                                      item.accepting = false;
+                                    });
+                                    playBeep('sounds/false.wav');
+                                  }
+                                },
+                                onWillAccept: (receivedItem) {
                                   setState(() {
-                                    items.remove(receivedItem);
-                                    items2.remove(item);
+                                    item.accepting = true;
                                   });
-                                  score += 10;
-                                  item.accepting = false;
-                                  playBeep('sounds/true.wav');
-                                  controller.play();
-                                  Timer(const Duration(seconds: 1),
-                                      (() => controller.stop()));
-                                } else {
+                                  return true;
+                                },
+                                onLeave: (receivedItem) {
                                   setState(() {
-                                    score -= 5;
                                     item.accepting = false;
                                   });
-                                  playBeep('sounds/false.wav');
-                                }
-                              },
-                              onWillAccept: (receivedItem) {
-                                setState(() {
-                                  item.accepting = true;
-                                });
-                                return true;
-                              },
-                              onLeave: (receivedItem) {
-                                setState(() {
-                                  item.accepting = false;
-                                });
-                              },
-                              builder:
-                                  (context, acceptedItems, rejectedItems) =>
-                                      Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: item.accepting
-                                      ? Colors.grey[400]
-                                      : Colors.grey[200],
+                                },
+                                builder:
+                                    (context, acceptedItems, rejectedItems) =>
+                                        Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: item.accepting
+                                        ? Colors.grey[400]
+                                        : Colors.grey[200],
+                                  ),
+                                  alignment: Alignment.center,
+                                  height:
+                                      MediaQuery.of(context).size.width / 6.5,
+                                  width:
+                                      MediaQuery.of(context).size.width / 3.2,
+                                  margin: const EdgeInsets.all(8),
+                                  child: TextButton(
+                                      onPressed: () {
+                                        playBeep(item.sound);
+                                      },
+                                      // ignore: prefer_const_constructors
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            item.name,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline6,
+                                          ),
+                                          const SizedBox(
+                                            width: 20,
+                                          ),
+                                          const Icon(Icons.volume_up_sharp),
+                                        ],
+                                      )),
                                 ),
-                                alignment: Alignment.center,
-                                height: MediaQuery.of(context).size.width / 6.5,
-                                width: MediaQuery.of(context).size.width / 3.2,
-                                margin: const EdgeInsets.all(8),
-                                child: TextButton(
-                                    onPressed: () {
-                                      playBeep(item.sound);
-                                    },
-                                    // ignore: prefer_const_constructors
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          item.name,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline6,
-                                        ),
-                                        const SizedBox(
-                                          width: 20,
-                                        ),
-                                        const Icon(Icons.volume_up_sharp),
-                                      ],
-                                    )),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        const Spacer(),
-                      ],
-                    ),
-                ],
-              ),
-              Positioned(
-                right: 10,
-                top: 40,
-                child: FloatingActionButton(
-                    backgroundColor: Colors.amber,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomePage()),
-                      );
-                    },
-                    child: const Icon(
-                      Icons.home_filled,
-                      color: Colors.blue,
-                      size: 50,
-                    )),
-              ),
-              if (lvl == 2)
-                Positioned(
-                  left: 0,
-                  top: 420,
-                  child: FloatingActionButton(
-                      onPressed: () {
-                        setState(() {
-                          initGame1();
-                        });
-                      },
-                      child: const Icon(Icons.navigate_before_sharp)),
+                              );
+                            }).toList(),
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                  ],
                 ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        Positioned(
+          right: 10,
+          top: 40,
+          child: FloatingActionButton(
+              backgroundColor: Colors.amber,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              child: const Icon(
+                Icons.home_filled,
+                color: Colors.blue,
+                size: 50,
+              )),
+        ),
+        if (lvl == 2)
+          Positioned(
+            left: 0,
+            top: MediaQuery.of(context).size.height * 0.5,
+            child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    initGame1();
+                  });
+                },
+                child: const Icon(Icons.navigate_before_sharp)),
+          ),
+      ],
+    )));
   }
 
   String result() {
