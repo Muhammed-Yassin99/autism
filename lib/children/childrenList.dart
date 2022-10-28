5e3ffe8667def71f806ff2411a7f9b9539035676
DrAaGoN <DrAaGoN@gmail.com>
2022-10-28 23:17:34 +0200

    commit

--- lib/children/childrenList.dart
+++ lib/children/childrenList.dart
@@ -4,9 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import '../model/drawer.dart';
-import 'basicTiles.dart';
 
 class childrenList extends StatefulWidget {
   const childrenList({super.key});
